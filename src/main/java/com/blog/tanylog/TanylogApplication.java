4cc028325107c243bfbeff31818444b3d56bea08
ByeoungJoon Jeon <juni8453@naver.com>
2023-07-26 17:31:31 +0900

    Tanylog Project 시작 !

---
+++ src/main/java/com/blog/tanylog/TanylogApplication.java
@@ -0,0 +1,13 @@
+package com.blog.tanylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TanylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TanylogApplication.class, args);
+	}
+
+}
