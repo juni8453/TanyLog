3fe55dbeb8155d7b3c02ee156de179fec630f9bb
ByeoungJoon Jeon <juni8453@naver.com>
2023-08-02 19:19:39 +0900

    ## Paging 성능 테스트 클래스 추가
    ### Description
    - 게시글 전체 조회 API 의 offset(), limit() 를 활용한 Paging 성능을 테스트하기 위한 테스트 클래스 추가

---
+++ src/test/java/com/blog/tanylog/post/service/PagingTest.java
@@ -0,0 +1,36 @@
+package com.blog.tanylog.post.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.blog.tanylog.post.controller.dto.request.PageSearch;
+import com.blog.tanylog.post.domain.Post;
+import com.blog.tanylog.post.repository.PostRepository;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagingTest {
+
+  @Autowired
+  private PostRepository postRepository;
+
+  @Test
+  @DisplayName("게시글 전체 조회 API 페이징 성능을 테스트합니다.")
+  void readAll() {
+    // given
+    PageSearch pageSearch = new PageSearch(1, 20);
+
+    // when
+    long startTime = System.currentTimeMillis();
+    List<Post> response = postRepository.readAll(pageSearch);
+    long endTime = System.currentTimeMillis();
+    long pagingTime = endTime - startTime;
+    System.out.println("반환 시간: " + pagingTime + "ms");
+
+    // then
+    assertThat(response).isNotNull();
+  }
+}
