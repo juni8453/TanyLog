71814dde82dd5087ce0e6257728ee1882a0bbef6
ByeoungJoon Jeon <juni8453@naver.com>
2023-08-09 20:50:55 +0900

    ## 게시글 Controller 테스트 클래스 생성
    ### Description
    - 게시글 API Controller 단 테스트를 작성하기 위한 클래스 생성

---
+++ src/test/java/com/blog/tanylog/post/controller/PostControllerTest.java
@@ -0,0 +1,28 @@
+package com.blog.tanylog.post.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PostControllerTest {
+
+  @Test
+  void save() {
+  }
+
+  @Test
+  void delete() {
+  }
+
+  @Test
+  void update() {
+  }
+
+  @Test
+  void read() {
+  }
+
+  @Test
+  void readAll() {
+  }
+}
