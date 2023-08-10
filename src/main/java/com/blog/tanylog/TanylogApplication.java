d2777bbb03d720a3cd6c5a62bdada1eefd8f6e84
ByeoungJoon Jeon <juni8453@naver.com>
2023-08-10 22:28:28 +0900

    ## JPA Auditing 설정 클래스 분리
    ### Description
    - 설정 클래스를 분리

--- src/main/java/com/blog/tanylog/TanylogApplication.java
+++ src/main/java/com/blog/tanylog/TanylogApplication.java
@@ -2,14 +2,12 @@ package com.blog.tanylog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class TanylogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TanylogApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(TanylogApplication.class, args);
+  }
 }
+
