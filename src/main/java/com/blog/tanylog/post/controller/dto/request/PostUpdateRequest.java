f1430c4a015762d75802f86eda771d11308f269c
ByeoungJoon Jeon <juni8453@naver.com>
2023-08-10 22:29:33 +0900

    ## Post Controller 테스트 작성
    ### Description
    - Service 에서 실시하지 않았던 비로그인 상태 리다이렉트 테스트 및 유효성 검사 테스트 실시
    - 테스트용 Security Cofing 파일 생성
    - Request DTO 에 @Size 어노테이션 추가

--- src/main/java/com/blog/tanylog/post/controller/dto/request/PostUpdateRequest.java
+++ src/main/java/com/blog/tanylog/post/controller/dto/request/PostUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.blog.tanylog.post.controller.dto.request;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostUpdateRequest {
 
+  @Size(max = 50, message = "제목은 50자를 넘길 수 없습니다.")
   @NotBlank(message = "제목을 비울 수 없습니다.")
   private String title;
 
