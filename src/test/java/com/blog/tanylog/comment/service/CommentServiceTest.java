d7c7177088d4a2d37adb8864e3ebdca58a6c5609
ByeoungJoon Jeon <juni8453@naver.com>
2023-08-14 14:58:12 +0900

    ## 대댓글 등록 Service Test 추가
    ### Description
    - 대댓글 작성 Test 추가
    - 대댓글 작성 예외 Test 추가

--- src/test/java/com/blog/tanylog/comment/service/CommentServiceTest.java
+++ src/test/java/com/blog/tanylog/comment/service/CommentServiceTest.java
@@ -1,6 +1,7 @@
 package com.blog.tanylog.comment.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import com.blog.tanylog.comment.controller.dto.request.CommentSaveRequest;
 import com.blog.tanylog.comment.domain.Comment;
@@ -8,6 +9,7 @@ import com.blog.tanylog.comment.repository.CommentRepository;
 import com.blog.tanylog.config.DatabaseCleanup;
 import com.blog.tanylog.config.WithMockCustomUser;
 import com.blog.tanylog.config.security.UserContext;
+import com.blog.tanylog.global.exception.domain.CommentDepthOverException;
 import com.blog.tanylog.post.domain.Post;
 import com.blog.tanylog.post.repository.PostRepository;
 import com.blog.tanylog.user.domain.Role;
@@ -61,6 +63,16 @@ class CommentServiceTest {
     post.addUser(user);
 
     postRepository.save(post);
+
+    Comment comment = commentRepository.save(Comment.builder()
+        .content("test content")
+        .isDeleted(false)
+        .build());
+
+    comment.addUser(user);
+    comment.addPost(post);
+
+    commentRepository.save(comment);
   }
 
   @AfterEach
@@ -88,8 +100,59 @@ class CommentServiceTest {
 
     // then
     List<Comment> findComments = commentRepository.findAll();
-    assertThat(findComments.size()).isEqualTo(1);
-    assertThat(findComments.get(0).getPost().getId()).isEqualTo(1L);
-    assertThat(findComments.get(0).getPost().getContent()).isEqualTo("dummy_content");
+    assertThat(findComments.size()).isEqualTo(2);
+    assertThat(findComments.stream().allMatch(comment -> comment.getReplyDepth() == 0)).isTrue();
+  }
+
+  @Test
+  @DisplayName("USER 권한의 유저는 대댓글을 등록할 수 있습니다.")
+  @WithMockCustomUser
+  void 대댓글_등록() {
+    // given
+    Long postId = 1L;
+    Long commentId = 1L;
+
+    UserContext userContext = (UserContext) SecurityContextHolder.getContext().getAuthentication()
+        .getPrincipal();
+
+    CommentSaveRequest request = CommentSaveRequest.builder()
+        .content("reply content")
+        .isDeleted(false)
+        .build();
+
+    // when
+    commentService.saveReply(postId, commentId, userContext, request);
+
+    // then
+    List<Comment> findComments = commentRepository.findAll();
+    assertThat(findComments.size()).isEqualTo(2);
+    assertThat(findComments.get(1).getParentComment().getId()).isEqualTo(1L);
+    assertThat(findComments.get(1).getReplyDepth()).isEqualTo(1);
+  }
+
+  @Test
+  @DisplayName("대댓글에는 대댓글을 작성할 수 없습니다.")
+  @WithMockCustomUser
+  void 무한_대댓글_작성_제한() {
+    // given
+    Long postId = 1L;
+    Long commentId = 1L;
+
+    UserContext userContext = (UserContext) SecurityContextHolder.getContext().getAuthentication()
+        .getPrincipal();
+
+    CommentSaveRequest request = CommentSaveRequest.builder()
+        .content("reply content")
+        .isDeleted(false)
+        .build();
+
+    // when
+    commentService.saveReply(postId, commentId, userContext, request);
+    List<Comment> comments = commentRepository.findAll();
+
+    // then
+    assertThrows(CommentDepthOverException.class,
+        () -> commentService.saveReply(postId, comments.get(comments.size() - 1).getId(),
+            userContext, request));
   }
 }
