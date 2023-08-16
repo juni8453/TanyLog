5c27a73cceaf62325d7e2f6531c2e50d0ad0fbba
ByeoungJoon Jeon <juni8453@naver.com>
2023-08-16 16:53:57 +0900

    ## OtherUser ~ Exception 메세지 수정
    ### Description
    - 게시글에서 글로 변경

--- src/main/java/com/blog/tanylog/global/exception/domain/OtherUserUpdateException.java
+++ src/main/java/com/blog/tanylog/global/exception/domain/OtherUserUpdateException.java
@@ -2,7 +2,7 @@ package com.blog.tanylog.global.exception.domain;
 
 public class OtherUserUpdateException extends GlobalException {
 
-  private static final String MESSAGE = "다른 유저의 게시글은 수정할 수 없습니다.";
+  private static final String MESSAGE = "다른 유저의 글은 수정할 수 없습니다.";
 
   public OtherUserUpdateException() {
     super(MESSAGE);
