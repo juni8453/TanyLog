ec555a3f22ca123547f9e979a9f9ee75f537733d
ByeoungJoon Jeon <juni8453@naver.com>
2023-08-22 18:08:47 +0900

    ## 좋아요 API 메서드 네이밍 변경, Service 로직 및 PostLike Entity 수정
    ### Description
    - save() 에서 saveOrDelete() 로 변경
    - findPostLike 변수로 한 번만 조회하고 사용하도록 변경
    - PostLike 의 user_id, post_id 에 unique 제약 조건 추가

--- src/main/java/com/blog/tanylog/postLike/service/PostLikeService.java
+++ src/main/java/com/blog/tanylog/postLike/service/PostLikeService.java
@@ -5,7 +5,7 @@ import com.blog.tanylog.global.exception.domain.PostNotFound;
 import com.blog.tanylog.global.exception.domain.UserNotFound;
 import com.blog.tanylog.post.domain.Post;
 import com.blog.tanylog.post.repository.PostRepository;
-import com.blog.tanylog.postLike.controller.dto.request.PostLikeSaveRequest;
+import com.blog.tanylog.postLike.controller.dto.request.PostLikeRequest;
 import com.blog.tanylog.postLike.domain.PostLike;
 import com.blog.tanylog.postLike.repository.PostLikeRepository;
 import com.blog.tanylog.user.domain.User;
@@ -24,20 +24,22 @@ public class PostLikeService {
   private final UserRepository userRepository;
 
   @Transactional
-  public void save(Long postId, UserContext userContext, PostLikeSaveRequest request) {
+  public void saveOrDelete(Long postId, UserContext userContext, PostLikeRequest request) {
     Long userId = userContext.getSessionUser().getUserId();
+
+    Optional<PostLike> findPostLike = postLikeRepository.findPostLikeByPostIdAndUserId(
+        postId, userId);
+
+    // 클라이언트에서 실수할수도 있으니 이중 검증
+    request.validate(findPostLike.orElse(null));
+
     User loginUser = userRepository.findById(userId)
         .orElseThrow(UserNotFound::new);
 
     Post findPost = postRepository.findById(postId)
         .orElseThrow(PostNotFound::new);
 
-    // 클라이언트에서 실수할수도 있으니 이중 검증
-    request.validate(postLikeRepository.findPostLikeByPostIdAndUserId(postId, userId)
-        .orElse(null));
-
     if (request.isLiked()) {
-      Optional<PostLike> findPostLike = postLikeRepository.findPostLikeByPostIdAndUserId(postId, userId);
       findPostLike.ifPresent(postLikeRepository::delete);
 
     } else {
