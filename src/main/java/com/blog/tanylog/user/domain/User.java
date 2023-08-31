d71c782c5889b4c81f924b4ef2445729fdad7827
Jeon(Tany) <79444040+juni8453@users.noreply.github.com>
2023-08-31 18:03:21 +0900

    배포 삽질 (#25)
    * Create gradle.yml
    
    * Update gradle.yml
    
    * ## 배포용 yml 파일 추가
    
    * ## 배포용 test yml 파일 추가
    
    * ## 배포 준비
    - 배포용 yml 수정
    - Dockerfile 생성
    - build.gradle 수정
    
    * ## GitHub Actions gradle.yml 수정
    
    * ## 자잘한 수정
    - yml, docs, User, DatabaseCleanup 수정
    
    * ## gradle.yml 수정
    
    * ## test 용 yml 파일 추가
    
    * ## gradle.yml 경로 수정
    
    * ## gradle.yml 수정
    
    * ## build.gradle 수정
    
    * ## build.gradle 수정 및 yml 수정
    
    * ## yml 수정
    
    * ## Security Config 주석 처리
    
    * ## Test 코드 주석처리
    
    * ## build.gradle 수정
    
    * ## 배포 테스트

--- src/main/java/com/blog/tanylog/user/domain/User.java
+++ src/main/java/com/blog/tanylog/user/domain/User.java
@@ -21,7 +21,6 @@ public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
-  @Column(name = "user_id")
   private Long id;
 
   private String oauthId;
--- src/test/java/com/blog/tanylog/config/DatabaseCleanup.java
+++ src/test/java/com/blog/tanylog/config/DatabaseCleanup.java
@@ -3,24 +3,24 @@ package com.blog.tanylog.config;
 import com.google.common.base.CaseFormat;
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.annotation.PostConstruct;
 import javax.persistence.Entity;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Table;
-import javax.transaction.Transactional;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
-public class DatabaseCleanup {
+public class DatabaseCleanup implements InitializingBean {
 
   @PersistenceContext
   private EntityManager entityManager;
 
   private List<String> tableNames;
 
-  @PostConstruct
-  public void initializeTableNames() {
+  @Override
+  public void afterPropertiesSet() {
     tableNames = entityManager.getMetamodel().getEntities().stream()
         .filter(e -> e.getJavaType().getAnnotation(Entity.class) != null)
         .map(e -> {
@@ -37,21 +37,14 @@ public class DatabaseCleanup {
   @Transactional
   public void execute() {
     entityManager.flush();
-
-    // 외래 키 제약 조건 해제
-    entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 0").executeUpdate();
+    entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
 
     for (String tableName : tableNames) {
-      // DELETE 문으로 레코드 삭제
-      entityManager.createNativeQuery("DELETE FROM " + tableName).executeUpdate();
-
-      // AUTO_INCREMENT 값을 초기화
+      entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
       entityManager.createNativeQuery(
-          "ALTER TABLE " + tableName + " AUTO_INCREMENT = 1").executeUpdate();
+          "ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
     }
 
-    // 외래 키 제약 조건 복원
-    entityManager.createNativeQuery("SET FOREIGN_KEY_CHECKS = 1").executeUpdate();
+    entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
   }
 }
-
